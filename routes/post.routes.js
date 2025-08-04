3f3259815e907da018307b844af1743cbcd4e5fa
afreenhaji <161109294+afreenhaji@users.noreply.github.com>
2025-08-04 07:55:10 +0530

    Your commit message here

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
-import { createPost, getAllPosts, getUserPosts} from "../controllers/post.Controllers.js";
+import { createPost, getAllPosts, getUserPosts } from "../controllers/postControllers.js";
 
 const router = express.Router();
 
 router.post("/create", isAuth, createPost);
 router.get("/feed", isAuth, getAllPosts);
 router.get("/user/:userId", isAuth, getUserPosts);
+
 export default router;
